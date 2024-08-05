70e1c4e6edf533da8c68b6e3ee84d96c24884d5a
Denis Strizhkin <strdenis02@gmail.com>
2024-08-06 00:15:21 +0300

    move to fish as login shell

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,42 @@
+# Source system profile
+if not type -q bass
+    set bass_path $HOME/.config/fish/bass
+    git clone --depth 1 https://github.com/edc/bass.git $bass_path
+    pushd $bass_path && make install && popd
+end
+bass source /etc/profile
+
+
+# Path
+set -gx PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.cargo/bin
+set -gx PATH $PATH $HOME/.local/share/flatpak/exports/bin
+
+# Kvantum
+set -gx QT_STYLE_OVERRIDE kvantum
+
+# Java
+set -gx _JAVA_AWT_WM_NONREPARENTING 1
+
+# Config home
+set -gx XDG_CONFIG_HOME $HOME/.config
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # GPG tui
+    set -gx GPG_TTY (tty)
+end
+
+if status is-login
+    # fcitx input method
+    set -gx GTK_IM_MODULE fcitx
+    set -gx QT_IM_MODULE fcitx
+    set -gx XMODIFIERS @im=fcitx
+
+    # setup runtime dir on openrc
+    if test -z $XDG_RUNTIME_DIR
+        set -gx XDG_RUNTIME_DIR=(mktemp -d /tmp/(id -u)-runtime-dir.XXX)
+    end
+
+    hx --grammar fetch && hx --grammar build
+    dbus-run-session river
 end
