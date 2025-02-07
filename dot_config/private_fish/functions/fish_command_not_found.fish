fe9297a563df652eeff405bc3886e63b7e7d6124
Denis Strizhkin <strdenis02@gmail.com>
2025-02-07 12:48:49 +0300

    command not found

--- dot_config/private_fish/functions/fish_command_not_found.fish
+++ dot_config/private_fish/functions/fish_command_not_found.fish
@@ -1,8 +1,19 @@
 function fish_command_not_found
-    set -l pkgs (apk search --quiet cmd:$argv[1])
-    set pkgs (string join '|' $pkgs)
-    echo "$argv[1]: not found"
+    set cmd $argv[1]
+
+    if type -q apk
+        set pkgs (apk search --quiet cmd:$cmd)
+        set istr "apk add"
+    end
+
+    if type -q emerge
+        set pkgs (e-file --plain $cmd | grep -E '^[^/]+/[^/]+$')
+        set istr "emerge --ask"
+    end
+
+    set pkgs (string join ' | ' $pkgs[..10])
+    echo "$cmd: not found"
     if test -n "$pkgs"
-        echo "  install with: apk add $pkgs"
+        echo "  install with: $istr [ $pkgs ]"
     end
 end
